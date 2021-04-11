f476dcdb7a0396c28d135aec5478fd0a436e2ba5
TmatzXzonEv <59580824+tmatzxzone@users.noreply.github.com>
2021-04-11 17:14:44 +0800

    Update availability.php

--- availability.php
+++ availability.php
@@ -37,8 +37,8 @@
 <script
   type="text/javascript"
   src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.js">
-						
 </script>
+
 
 
 <!-- ######################### Availability ######################### -->
@@ -49,32 +49,53 @@
 			<!-- Search bar -->
 			<form action="" class="my-5 pb-5 d-flex justify-content-center" method="GET">
 				<div class="row">
-					<div class="col-md-5 my-n3">
+					<div class="col-1"></div>
+					<div class="col-md-3">
 						<div class="form-group">
 							<span class="form-label" style="color:white;">Date</span>
 							<input id="date" class="form-control" min="<?php echo date('Y-m-d'); ?>" type="date" name="date" required>
 						</div>
 					</div>
 
-					<div class="col-md-5 ">
-						<div class="input-group my-2">	
-							<select class="browser-default custom-select" name="sess" id="sess">
-							  <option disabled selected>Select Session</option>
-							  <option value="08:00 - 09:00">08:00 - 09:00</option>
-							  <option value="09:00 - 10:00">09:00 - 10:00</option>
-							  <option value="10:00 - 11:00">10:00 - 11:00</option>
-							  <option value="11:00 - 12:00">11:00 - 12:00</option>
-							  <option value="12:00 - 13:00">12:00 - 13:00</option>
-							  <option value="14:00 - 15:00">14:00 - 15:00</option>
-							  <option value="15:00 - 16:00">15:00 - 16:00</option>
-							  <option value="16:00 - 17:00">16:00 - 17:00</option>
-							  <option value="17:00 - 18:00">17:00 - 18:00</option>
-							</select>
-			              </div>
-			        </div>
+					<div class="col-md-3">
+						<div class="form-group">
+							<span class="form-label" style="color:white;">Start Time</span>
+							<div class="input-group clockpicker">
+								<input type="text" id="startTime" class="form-control" value="00:00" name="startTime">
+							</div>
+							<script type="text/javascript">
+								$('.clockpicker').clockpicker({autoclose: 'True'});
+								 var elem = $('#startTime');
+								 var c = elem.clockpicker({autoclose: 'True', align: 'top', placement: 'bottom',
+								 	afterHourSelect: function(){
+								    	c.data('clockpicker').done();
+								    }
+								 });
+								</script>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="form-group">
+							<span class="form-label" style="color:white;">End Time</span>
+							<div class="input-group clockpicker">
+								<input type="text" id="endTime" class="form-control" value="00:00" name="endTime">
+							</div>
+							<script type="text/javascript">
+							$('.clockpicker').clockpicker({autoclose: 'True'});
+							 var elem2 = $('#endTime');
+							 var d = elem2.clockpicker({autoclose: 'True', align: 'top', placement: 'bottom',
+							 	afterHourSelect: function(){
+							    	d.data('clockpicker').done();
+							    }
+							 });
+							</script>
+						</div>
+					</div>
+
 					<div class="col-md-1">
 						<div class="form-group">
-							<button type="submit" name="submit" class="btn btn-primary my-2" value="search">
+							<button type="submit" name="submit" class="btn btn-primary my-4" value="search">
 								<i class="fas fa-search"></i>
 							</button>
 						</div>
@@ -83,7 +104,6 @@
 			</form>
 			<!-- End of Search Bar -->
 
-			<!-- Load Table -->
 			<div class="table-responsive-lg" id="table_loader">
 				<script type="text/javascript">
 					$(document).ready(function() {
@@ -92,5 +112,5 @@
 				</script>
 			</div>
 		</div>
-	</div>
-<!-- ######################### End Of Availability ######################### -->
+	</div>	
+<!-- ######################### Availability ######################### -->
